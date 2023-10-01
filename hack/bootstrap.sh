d751adf558d83355440775e517829b1485a92b0b
Dharmit Shah <shahdharmit@gmail.com>
2023-10-01 12:14:19 +0530

    Create rpmtree for generic sidecar
    Signed-off-by: Dharmit Shah <shahdharmit@gmail.com>

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -26,7 +26,7 @@ KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 HOST_ARCHITECTURE="$(uname -m)"
 
 sandbox_root=${SANDBOX_DIR}/default/root
-sandbox_hash="efe043bfc797b7ddacf24a7d9b36faf971835212"
+sandbox_hash="dd62eda05be62bc0fc33655aacdad675f3571a96"
 
 function kubevirt::bootstrap::regenerate() {
     (
